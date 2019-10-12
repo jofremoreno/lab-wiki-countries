b32062287ebedd25db11d854bda4d7e75c7909e6
Jofre <jofremoreno@gmail.com>
2019-10-12 06:30:19 +0200

    done

---
+++ src/CountryDetail.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class CountryDetail extends Component {
+  render() {
+    return (
+      <NavLink
+        to={this.props.href}
+        activeClassName="active"
+        className="list-group-item list-group-item-action"
+      >
+        {this.props.flag}
+        {this.props.children}
+      </NavLink>
+    );
+  }
+}
